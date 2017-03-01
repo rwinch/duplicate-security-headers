d565d38b4afe5ecbedd77d63d42f70c96a080999
Rob Winch <rwinch@gopivotal.com>
2017-02-28 21:55:31 -0600

    Remove Security

--- src/main/java/com/example/DuplicateSecurityHeadersApplication.java
+++ src/main/java/com/example/DuplicateSecurityHeadersApplication.java
@@ -1,13 +1,19 @@
 package com.example;
 
+import java.io.IOException;
+
 import javax.servlet.AsyncContext;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 @SpringBootApplication
 @RestController
@@ -31,4 +37,15 @@ public class DuplicateSecurityHeadersApplication {
 		context.dispatch("/request");
 	}
 
+	@Bean
+	public OncePerRequestFilter doOnceFilter() {
+		return new OncePerRequestFilter() {
+			@Override
+			protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+					throws ServletException, IOException {
+				response.addHeader("X-Only-Once", "Here");
+				filterChain.doFilter(request, response);
+			}
+		};
+	}
 }
